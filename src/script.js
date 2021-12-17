2e1bfe67945ff385e882b9f4bd9af616d42cde68
BrittHellinckx <britt.hellinckx@student.ehb.be>
2021-12-17 20:11:21 +0100

    Setting up functions for sub navigation on profile

--- src/script.js
+++ src/script.js
@@ -1,55 +1,50 @@
 "use strict";
-
 import _ from 'lodash';
 
-
 window.onload = function () {
-    //RANDOM PHOTO
-    document.getElementById('photoBtn').addEventListener('click', e => {
-        e.preventDefault();
-        let randomNumber = Math.floor(Math.random() * 50);
-        randomPhoto(randomNumber);
-    })
+    if (window.location.pathname == '/docs/pages/generator.html') {
+        //RANDOM PHOTO
+        document.getElementById('photoBtn').addEventListener('click', e => {
+            e.preventDefault();
+            let randomNumber = Math.floor(Math.random() * 50);
+            randomPhoto(randomNumber);
+        })
 
-    //RANDOM COLOURS
-    document.getElementById('colourBtn').addEventListener('click', e => {
-        e.preventDefault();
-        let random1 = Math.floor(Math.random() * 0xffffff).toString(16);
-        let random2 = Math.floor(Math.random() * 0xffffff).toString(16);
-        let random3 = Math.floor(Math.random() * 0xffffff).toString(16);
-        let random4 = Math.floor(Math.random() * 0xffffff).toString(16);
-        randomColours(random1, random2, random3, random4);
-    })
-
-    //PROFILE SELECTED SUBNAVIGATION
-    document.getElementById('savedBtn').addEventListener('click', e => {
-        e.preventDefault();
-        saved();
-    });
-    document.getElementById('artBtn').addEventListener('click', e => {
-        e.preventDefault();
-        art();
-    });
-    document.getElementById('postsBtn').addEventListener('click', e => {
-        e.preventDefault();
-        posts();
-    });
-    document.getElementById('likedBtn').addEventListener('click', e => {
-        e.preventDefault();
-        liked();
-    });
-
+        //RANDOM COLOURS
+        document.getElementById('colourBtn').addEventListener('click', e => {
+            e.preventDefault();
+            let random1 = Math.floor(Math.random() * 0xffffff).toString(16);
+            let random2 = Math.floor(Math.random() * 0xffffff).toString(16);
+            let random3 = Math.floor(Math.random() * 0xffffff).toString(16);
+            let random4 = Math.floor(Math.random() * 0xffffff).toString(16);
+            randomColours(random1, random2, random3, random4);
+        })
+    } else if (window.location.pathname == '/docs/pages/profile.html') {
+        //PROFILE SELECTED SUBNAVIGATION
+        document.getElementById('savedBtn').addEventListener('click', e => {
+            e.preventDefault();
+            saved();
+        });
+        document.getElementById('artBtn').addEventListener('click', e => {
+            e.preventDefault();
+            art();
+        });
+        document.getElementById('postsBtn').addEventListener('click', e => {
+            e.preventDefault();
+            posts();
+        });
+        document.getElementById('likedBtn').addEventListener('click', e => {
+            e.preventDefault();
+            liked();
+        });
+    }
 }
-
 
 //RANDOM PHOTO
 async function randomPhoto(number) {
-    console.log(number);
     fetch(`https://picsum.photos/id/${number}/info`)
         .then(response => response.json())
         .then(function (data) {
-            console.log(data)
-
             document.getElementById('genBtn').style.display = "none"
             document.getElementById('colour').style.display = "none"
             document.getElementById('photo').innerHTML = `<h2>Here is your inspiration for your next masterpiece</h2>
@@ -62,8 +57,6 @@ async function randomPhoto(number) {
 
 //RANDOM COLOURS
 async function randomColours(c1, c2, c3, c4) {
-    console.log(c1, c2, c3, c4)
-
     document.getElementById('genBtn').style.display = "none"
     document.getElementById('photo').style.display = "none"
     document.getElementById('colour').innerHTML = `<h2>Here is your inspiration for your next masterpiece</h2>
@@ -73,27 +66,38 @@ async function randomColours(c1, c2, c3, c4) {
                                                         <img src="http://www.thecolorapi.com/id?format=svg&hex=${c3}">
                                                         <img src="http://www.thecolorapi.com/id?format=svg&hex=${c4}">
                                                     </div>
-                                                    <button id="Save">Save</button>`
+                                                    <button id="save">Save</button>`
 }
 
 //PROFILE SELECTED SUBNAVIGATION
-async function saved() {
-    console.log("saved")
+function saved() {
+    //Displaying correct section
+    document.getElementById('saved').style.display = "initial";
+    document.getElementById('art').style.display = "none";
+    document.getElementById('posts').style.display = "none";
+    document.getElementById('liked').style.display = "none";
 }
 
 function art() {
-    console.log("art")
+    //Displaying correct section
+    document.getElementById('art').style.display = "initial";
+    document.getElementById('saved').style.display = "none";
+    document.getElementById('posts').style.display = "none";
+    document.getElementById('liked').style.display = "none";
 }
 
 function posts() {
-    console.log("posts")
+    //Displaying correct section
+    document.getElementById('posts').style.display = "initial";
+    document.getElementById('art').style.display = "none";
+    document.getElementById('saved').style.display = "none";
+    document.getElementById('liked').style.display = "none";
 }
 
 function liked() {
-    console.log("liked")
+    //Displaying correct section
+    document.getElementById('liked').style.display = "initial";
+    document.getElementById('art').style.display = "none";
+    document.getElementById('posts').style.display = "none";
+    document.getElementById('saved').style.display = "none";
 }
-
-
-
-//event.target.closest('.?').id .? = id 
-//event.target.classname.indexOf('?') !== -1
